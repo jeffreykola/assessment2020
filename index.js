914199832525a8720e642764499070e84ec584ca
Jeffrey2000 <jeffltd@outlook.com>
2020-03-11 19:26:22 +0000

    general

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-
+    //Swiper from swiper.js used for viewing tasks
     const mySwiper = new Swiper('.swiper-container', {
         slidesPerView: 'auto',
-        spaceBetween: 10,
         scrollbar: {
             el: '.swiper-scrollbar',
         },
     });
 
 
+    //Datetime picker from flactpickr
     let date = new Date();
     const loadedTime = date.getHours() + ":" + date.getMinutes();
 
@@ -18,5 +18,18 @@ $(document).ready(function() {
         altInput: true,
         minDate: "today",
         minTime: loadedTime
+    });
+
+    //Handle the use of the images as buttons
+
+    let alreadyClicked = false;
+
+    $('#edit').click(function() {
+        alreadyClicked = true;
+        let checked = [];
+        $('input[type="checkbox"]:checked').each(function() {
+            checked.push($(this).attr('name'));
+            console.log(checked);
+        });
     });
 })
