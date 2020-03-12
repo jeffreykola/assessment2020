1d63711c9ea1b1ae7dda2f208a041d44ac09844f
Jeffrey2000 <jeffltd@outlook.com>
2020-03-12 02:03:49 +0000

    tried my best to do a bit of state management for buttons and checkboxes etc, this part of the SPA is now ready for back end functionality

--- index.js
+++ index.js
@@ -20,16 +20,50 @@ $(document).ready(function() {
         minTime: loadedTime
     });
 
-    //Handle the use of the images as buttons
+    //State management
 
-    let alreadyClicked = false;
+
+    let checked = new Set();
+    var editDisabled = false;
+    var checkboxesEnabled = false;
+    let checkedBoxes = Array.from(checked);
 
     $('#edit').click(function() {
-        alreadyClicked = true;
-        let checked = [];
+
         $('input[type="checkbox"]:checked').each(function() {
-            checked.push($(this).attr('name'));
-            console.log(checked);
+            checked.add($(this).attr('name'));
         });
+
+
+        editDisabled = true;
+        checkboxesEnabled = true;
+
+        if (checked.size > 0) {
+            checkedBoxes = Array.from(checked);
+
+            for (let i = 0; i < checkedBoxes.length; i++) {
+                const previousValue = $('.' + checkedBoxes[i]).text();
+                $('.' + checkedBoxes[i]).html('<input class="' + checkedBoxes[i] + '_input" type="text" value="' + previousValue + '"/>');
+            }
+
+
+            $('input:checkbox').attr('disabled', 'true');
+            $('#edit').css({ 'pointer-events': 'none' });
+        }
     });
-})
+
+    $('.save_button').click(function() {
+        if (editDisabled && checkboxesEnabled && checkedBoxes.length > 0) {
+            $('input:checkbox').removeAttr('disabled');
+            $('#edit').css({ 'pointer-events': 'none' });
+
+            $('.subject-1:input').click(() => { alert("Hello"); });
+            for (let i = 0; i < checkedBoxes.length; i++) {
+                const currentValue = $('.' + checkedBoxes[i] + '_input').val();
+                console.log(currentValue);
+                $('.' + checkedBoxes[i]).html('<span>' + currentValue + '</span>');
+            }
+        }
+    });
+
+});
