7a878eb71470e5109fbc6011a0311355ca45ef73
Jeffrey2000 <jeffreykola15@gmail.com>
2020-03-29 23:23:38 +0100

    working on back end

---
+++ server.js
@@ -0,0 +1,43 @@
+const fetch = require('node-fetch');
+const express = require('express');
+const app = express();
+const Datastore = require('nedb');
+
+const http = require('http');
+//const fs = require('fs');
+const bodyParser = require("body-parser");
+app.use(express.static('assets'));
+app.use(express.static('styles'));
+app.use(express.static('./'));
+//Find out what this means
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+const database = new Datastore('taskDatabase.db');
+database.loadDatabase();
+
+//POST route for adding a task
+app.post('/', (req,res)=>{
+    const data = req.body;
+    database.insert(data);
+    res.sendStatus(200);
+    console.log(`Added a task to the database`);
+});
+
+//GET route for getting all the tasks
+
+app.get('/', (req, res)=> {  
+    database.find({}, (error, data)=>{ 
+        if (error){
+            res.json({'code': 600});
+            res.end();
+        }
+        res.json(data);
+    });
+});
+
+app.listen(3000);
+
+
